598a86af7b4cb6b1d4f63b112111a4009bff1d6e
jMoscato <110347584+moscato@users.noreply.github.com>
2022-07-31 14:16:05 -0600

    Update laptop.ps1

--- laptop.ps1
+++ laptop.ps1
@@ -4,9 +4,7 @@
   $ser = Get-WmiObject Win32_BIOS | % {$_.SerialNumber}
   Start-Transcript -append -Path ".\info\$ser -info.txt"
 
-#Start-Transcript -append -Path "D:\$mod -info.txt"
 
-#out-file E:\info.txt -encoding UTF8
 Write-Host ' '
 Write-Host "Info -- *LapTop Edition!*" -foregroundcolor green
 Write-Host "---------------------------------" -foregroundcolor green
@@ -34,7 +32,6 @@ Get-WmiObject Win32_ComputerSystem | ? {Write-Host $_.Model}
   
   Write-Host "$($ssFin)`" HD LCD Screen ($($horz) x $($verz))"
   
-				#Get-WmiObject Win32_VideoController | ? {Write-Host ($ssFin)'" HD LCD Screen ('$_.CurrentHorizontalResolution 'x' $_.CurrentVerticalResolution')'}
 
   $cpuName = Get-WmiObject Win32_processor | % {$_.name}
   $cores = Get-WmiObject Win32_processor | % {$_.NumberOfCores}
@@ -42,7 +39,6 @@ Get-WmiObject Win32_ComputerSystem | ? {Write-Host $_.Model}
   
 Write-Host $cpuName.Replace("(R)", "").Replace("(TM)", "").Replace(" CPU", "").Replace("@ ", "@") "($($cores) Cores $($threads) Threads)"
 
-				#Get-wmiObject win32_processor | ? {Write-Host $_.name "|" $_.NumberOfCores "Cores" $_.NumberOfLogicalProcessors"Threads"}
 
   $memTotal = Get-WmiObject Win32_physicalMemory | % {($_.capacity/1gb)}
   $memSlots = Get-WmiObject Win32_physicalMemoryArray | % {$_.Memorydevices}
@@ -50,14 +46,13 @@ Write-Host $cpuName.Replace("(R)", "").Replace("(TM)", "").Replace(" CPU", "").R
 
 Write-Host "$($memTotal) GB $($ddr) ($($memFirst) GB x $($memSlots) Slots)"
 
-				#Write-Host Get-WmiObject Win32_PhysicalMemory | ? {Write-Host ($_.capacity/1gb) "GB" $ddr}
 				
-  $vc = Get-WmiObject win32_videocontroller | %  {$_.Name}
+$vc = Get-WmiObject win32_videocontroller | %  {$_.Name}
 
 Write-Host $vc.Replace("(R)", "").Replace("(TM)", "")
 
   $stor = Get-WmiObject Win32_LogicalDisk | ? {$_.DeviceId -eq "C:"} | % {"{0:N0}" -f ($_.Size/1gb)}
-				#$stor = Get-WmiObject Win32_LogicalDisk | ? {($_.Size/1gb) -gt 100} | % {"{0:N0}" -f $_.Size/1gb)}
+
   $dt = Get-PhysicalDisk | % { $_.MediaType }
   $dtFin = $dt[0]
   
@@ -79,7 +74,6 @@ Write-Host $vc.Replace("(R)", "").Replace("(TM)", "")
 	  Write-Host $stor "GB" $dtFin
   }
   
-		#Write-Host $stor "GB" $dtFin
 
 
   $Vers = Get-Ciminstance Win32_OperatingSystem | % caption
@@ -114,8 +108,7 @@ Start-Process devmgmt.msc
 Write-Host 'Device manager is open' -foregroundcolor yellow
 Write-Host "File Explorer Options is open to clear recent history" -foregroundcolor yellow
 Write-Host "^^explorer is open to prove this +-- Testing purposes --+" -foregroundcolor yellow
-            # Start-Process ms-settings:activation
-            # Write-Host "Windows activation is open" -foregroundcolor yellow
+
 Start-Process explorer.exe
 Write-Host "Network Status opened for internet information." -foregroundcolor yellow
 Start-Process "ms-settings:network-status"
